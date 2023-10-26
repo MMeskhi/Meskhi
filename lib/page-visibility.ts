06348620ff6a6af3ddd5e92c9a94b8004b8e54cb
Mikheil Meskhi <mishameskhi1@gmail.com>
2023-10-26 20:15:37 +0400

    fixed document ssr issue

--- lib/page-visibility.ts
+++ lib/page-visibility.ts
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function usePageVisibility(): boolean {
-  const [isVisible, setIsVisible] = useState(!document.hidden);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
+    setIsVisible(!document.hidden);
     const handleVisibilityChange = () => {
       setIsVisible(!document.hidden);
     };
-
     document.addEventListener("visibilitychange", handleVisibilityChange);
-
     return () => {
       document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
